0aef0b1be0f876e342fde611246f0c9cfcb6fb65
mperdeck <mperdeck@gmail.com>
2014-04-25 19:01:55 +1000

    CommonLogging 2.6.7
    Tested against site that logs from C# through Common.Logging.

--- JSNLog.CommonLogging/install.ps1
+++
@@ -1,5 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
- 
-$project.ProjectItems | ForEach { if ($_.Name -eq "Installation Dummy - reopen solution to will disappear on solutionFile.txt") { $_.Remove() } }
-$projectPath = Split-Path $project.FullName -Parent
-Join-Path $projectPath "Scripts\InstallationDummyFile.txt" | Remove-Item
