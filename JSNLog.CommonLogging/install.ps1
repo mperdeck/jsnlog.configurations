7dccb2051697c280297ba89ff680b5cdf6dbcc58
mperdeck <mperdeck@gmail.com>
2014-04-25 17:37:43 +1000

    Intermediate commit

--- JSNLog.CommonLogging/install.ps1
+++ JSNLog.CommonLogging/install.ps1
@@ -1,5 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
  
-$project.ProjectItems | ForEach { if ($_.Name -eq "InstallationDummyFile.txt") { $_.Remove() } }
+$project.ProjectItems | ForEach { if ($_.Name -eq "Installation Dummy - reopen solution to will disappear on solutionFile.txt") { $_.Remove() } }
 $projectPath = Split-Path $project.FullName -Parent
-Join-Path $projectPath "InstallationDummyFile.txt" | Remove-Item
+Join-Path $projectPath "Scripts\InstallationDummyFile.txt" | Remove-Item
