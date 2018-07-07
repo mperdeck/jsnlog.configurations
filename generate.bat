6e9da365ffd3129015aeb5fb31d4b824e9164ee9
mperdeck@gmail.com <mperdeck@gmail.com>
2018-07-07 22:20:23 +1000

    generation of most config nuget packages

---
+++ generate.bat
@@ -0,0 +1,14 @@
+rem Before running this batch file, install wyam
+rem https://wyam.io
+rem
+rem During a build, first process all .template files (replacing __Version__ with final version), then run this file.
+
+wyam --input .\Source --output .\FinalPackages
+
+copy Source\readme.txt FinalPackages\JSNLog.Elmah
+copy Source\readme.txt FinalPackages\JSNLog.Log4Net
+copy Source\readme.txt FinalPackages\JSNLog.NLog
+copy Source\readme.txt.serilog FinalPackages\JSNLog.Serilog
+
+copy Source\web.config.transform.nlog FinalPackages\web.config.transform
+
