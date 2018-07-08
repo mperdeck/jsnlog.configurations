482f7624212f8e22e3aaac104ccc839c95c0c499
mperdeck@gmail.com <mperdeck@gmail.com>
2018-07-08 11:25:45 +1000

    onepackage - xcopy content dir to the packages

--- generate.bat
+++ generate.bat
@@ -10,5 +10,10 @@ copy Source\readme.txt FinalPackages\JSNLog.Log4Net
 copy Source\readme.txt FinalPackages\JSNLog.NLog
 copy Source\readme.txt.serilog FinalPackages\JSNLog.Serilog\readme.txt
 
+xcopy  Source\content\*.* FinalPackages\JSNLog.Elmah\content\*.* /e /q
+xcopy  Source\content\*.* FinalPackages\JSNLog.Log4Net\content\*.* /e /q
+xcopy  Source\content\*.* FinalPackages\JSNLog.NLog\content\*.* /e /q
+xcopy  Source\content\*.* FinalPackages\JSNLog.Serilog\content\*.* /e /q
+
 copy Source\web.config.transform.nlog FinalPackages\JSNLog.NLog\web.config.transform
 
